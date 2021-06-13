00a564a4c51eefff396c086e09767adefa3b7b95
jokkepokke <59065776+jokkepokke@users.noreply.github.com>
2021-06-13 20:47:17 +0300

    fxmanifest

---
+++ hujo_tint/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Hujo'
+description 'change weapon tint by usable item'
+version '1.0.0'
+
+-- What to run
+client_script 'client.lua'
+
+server_script 'server.lua'
